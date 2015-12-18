39580b308eaa6f75b44a27f4c5ee80870cc4955a
Michael Bottone <bottonedev@gmail.com>
2015-12-18 15:30:22 -0500

    More refactoring

--- PegSolitate/main.swift
+++ PegSolitate/main.swift
@@ -8,6 +8,52 @@
 
 import Foundation
 
+func createGraph(node: BoardNode) -> Bool
+{
+    let moves = node.generateMoves()
+    
+    for moveNode in moves
+    {
+        if createGraph(moveNode)
+        {
+            node.moves.append(moveNode)
+        }
+    }
+    
+    return (node.board.pegCount == 1 || node.moves.count != 0)
+}
+
+var outputString = "<root>\n"
+func generateOutputString(state: BoardNode, height: Int)
+{
+    if height != 0
+    {
+        for _ in 1...height
+        {
+            outputString += "   "
+        }
+        outputString += "<move>\(state.moveString)\n"
+    }
+    
+    for move in state.moves
+    {
+        generateOutputString(move, height: height + 1)
+    }
+    
+    if height != 0
+    {
+        for _ in 1...height
+        {
+            outputString += "   "
+        }
+        outputString += "</move>\n"
+    }
+    else
+    {
+        outputString += "</root>\n"
+    }
+}
+
 let data: [[BoardValue]] = [
     [.Empty, .Peg, .Peg, .Peg, .Peg],
     [.Peg, .Peg, .Peg, .Peg],
